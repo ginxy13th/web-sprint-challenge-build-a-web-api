2d41b20d45140f9c4a90e6c74b32acf1b1ca5f0d
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-09-04 13:37:44 -0400

    grr

--- middleware.js
+++ middleware.js
@@ -1,25 +1,39 @@
 module.exports = {
     validateProjectBody: () => {
         return (req, res, next) => {
-            if (!req.body.name || !req.body.description) {
-                res.status(400).json({message: 'missing some information'})
-            } else if (req.body) {
-                next()
-            } else {
+            const body = req.body
+            .then(response => {
+                if (!body.name || !body.description) {
+                    res.status(400).json({message: 'missing some information'})
+                } else if (body) {
+                    res.status(200).json({message: 'success'})
+                    next()
+                } else {
+                    res.status(404).json({message: 'not found'})
+                }
+            })
+            .catch(err => {
                 next(err)
-            }
+            })
         }
     },
 
     validateActionBody: () => {
         return (req, res, next) => {
-            if (!req.body.description || !req.body.notes) {
-                res.status(400).json({message: 'missing some information'})
-            } else if (req.body) {
-                next()
-            } else {
+            const body = req.body
+            .then(response => {
+                if (!body.description || !body.notes || !body.project_id) {
+                    res.status(400).json({message: 'missing some information'})
+                } else if (body) {
+                    res.status(200).json({message: 'success'})
+                    next()
+                } else {
+                    res.status(404).json({message: 'not found'})
+                }
+            })
+            .catch(err => {
                 next(err)
-            }
+            })
         }
     }
 }
